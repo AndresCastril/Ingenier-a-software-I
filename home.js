07f3435e09f39a2ecdf420d4519da531578ac074
Andres54 <andrescastrillon2005@gmail.com>
2025-02-26 16:32:41 -0500

    Se arregló error de sintaxis

--- home.js
+++ home.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     Swal.fire({
         title: "¡Bienvenido a BrainBook!",
-        text: "",
         icon: "info",
         confirmButtonColor: "#394a5c"
     });
